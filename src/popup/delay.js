3c5af8fd18407d5ec4cad8707d32d92066bad9ad
Nigecat <nigel.quick@carey.com.au>
2019-11-26 13:18:44 +1100

    Overhauled delay slider

--- src/popup/delay.js
+++ src/popup/delay.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {        // Runs when the tab is open
         delay = data.delay
     });
     new Promise(resolve => setTimeout(resolve, 100)).then(() => {        // Give it time to retrive the data
-        slider.step = "50";
+        slider.step = "25";
         if (delay != undefined) {
             slider.value = delay
         }
@@ -19,18 +19,31 @@ window.addEventListener('load', function() {        // Runs when the tab is open
 })
 
 // Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    document.getElementById("delayVal").innerHTML = `Delay: ${this.value}ms`;
-    try {
-        chrome.storage.local.remove("delay");    // This will only have an error when the user is new to the program because the key won't exist in the user's localstorage
-    } catch (err) {
-        console.log(err)
-    }
+if (slider) {
+    slider.oninput = function() {
+        document.getElementById("delayVal").innerHTML = `Delay: ${this.value}ms`;
+        try {
+            chrome.storage.local.remove("delay");    // This will only have an error when the user is new to the program because the key won't exist in the user's localstorage
+        } catch (err) {
+            console.log(err)
+        }
 
-    var delay = document.getElementById("delayVal").innerHTML.split("ms").join("").split(" ")[1];   // Pull the current text from the input field and format it so it is just the number without the other text (it’s janky but it works)
-    chrome.storage.local.set({'delay': delay}, function () {      // Save the new delay
-        console.log("Delay saved as " + delay + "ms")
-    });
+        
+        var filter = getComputedStyle(document.getElementById("delay")).getPropertyValue('--slider-filter');
+        filter = filter.split("deg");
+        filter[0] = filter[0].split("(");
+        filter[0][1] = (parseInt(filter[0][1]) + 4).toString();
+
+        filter[0] = filter[0].join("(");
+        filter = filter.join("deg");
+
+        document.getElementById("delay").style.setProperty('--slider-filter', filter);
+
+        var delay = document.getElementById("delayVal").innerHTML.split("ms").join("").split(" ")[1];   // Pull the current text from the input field and format it so it is just the number without the other text (it’s janky but it works)
+        chrome.storage.local.set({'delay': delay}, function () {      // Save the new delay
+            console.log("Delay saved as " + delay + "ms")
+        });
+    }
 }
 
 
@@ -38,7 +51,9 @@ slider.oninput = function() {
 
 var rangeSlider = document.getElementById("rs-range-line");
 var rangeBullet = document.getElementById("rs-bullet");
-rangeSlider.addEventListener("input", showSliderValue, false);
+if (rangeSlider) {
+    rangeSlider.addEventListener("input", showSliderValue, false);
+}
 function showSliderValue() {
   rangeBullet.innerHTML = rangeSlider.value;
   var bulletPosition = (rangeSlider.value /rangeSlider.max);
